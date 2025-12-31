58aeb5f0dbd2bcf92b3df4c17fa9277776d986c4
jackluo <net.webjoy@gmail.com>
2025-12-31 15:50:47 +0800

    feat: 集成 rustdx 获取实时行情

--- services/data-collector/src/main.rs
+++ services/data-collector/src/main.rs
@@ -1,6 +1,8 @@
 // services/data-collector/src/main.rs
 use anyhow::Result;
 use tracing::{info, error};
+use rustdx_complete::tcp::{Tcp, Tdx};
+use rustdx_complete::tcp::stock::SecurityQuotes;
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -13,7 +15,36 @@ async fn main() -> Result<()> {
 
     info!("数据采集服务启动");
 
-    // TODO: 连接通达信服务器
+    // 连接通达信服务器
+    let mut tcp = match Tcp::new() {
+        Ok(t) => {
+            info!("成功连接到通达信服务器");
+            t
+        }
+        Err(e) => {
+            error!("连接通达信服务器失败: {}", e);
+            return Err(e.into());
+        }
+    };
+
+    // 测试获取股票行情
+    let mut quotes = SecurityQuotes::new(vec![
+        (0, "000001"),  // 平安银行
+        (1, "600000"),  // 浦发银行
+    ]);
+
+    match quotes.recv_parsed(&mut tcp) {
+        Ok(_) => {
+            for quote in quotes.result() {
+                info!("{} {} 价格:{} 涨跌幅:{}%",
+                    quote.code, quote.name, quote.price, quote.change_percent);
+            }
+        }
+        Err(e) => {
+            error!("获取行情失败: {}", e);
+        }
+    }
+
     // TODO: 推送数据到 Redis Stream
 
     Ok(())
