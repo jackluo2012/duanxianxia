1c4a1bc8cf311572f59b415729288421df12513d
jackluo <net.webjoy@gmail.com>
2025-12-31 15:41:06 +0800

    feat: 数据采集服务 - 基础框架

---
+++ services/data-collector/src/main.rs
@@ -0,0 +1,21 @@
+// services/data-collector/src/main.rs
+use anyhow::Result;
+use tracing::{info, error};
+use tracing_subscriber;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    // 初始化日志
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::INFO)
+        .with_target(false)
+        .json()
+        .init();
+
+    info!("数据采集服务启动");
+
+    // TODO: 连接通达信服务器
+    // TODO: 推送数据到 Redis Stream
+
+    Ok(())
+}
