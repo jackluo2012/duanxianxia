b5c9b2be18215278ee061cf658857dbe1b598b1e
jackluo <net.webjoy@gmail.com>
2026-01-14 10:40:03 +0800

    feat: 添加龙头高度主页面

--- frontend/src/pages/LeaderPage.tsx
+++ frontend/src/pages/LeaderPage.tsx
@@ -1,8 +1,44 @@
-export default function LeaderPage() {
+import { Row, Col, Typography } from 'antd';
+import { useLeaderStore } from '../store/leaderStore';
+import FilterBar from '../components/leader/FilterBar';
+import LeaderBoard from '../components/leader/LeaderBoard';
+import LeaderDetail from '../components/leader/LeaderDetail';
+import type { LeaderBoardItem } from '../types/leader';
+
+const { Title } = Typography;
+
+function LeaderPage() {
+  const { selectedStock, setSelectedStock, addComparedStock } = useLeaderStore();
+
+  const handleStockSelect = (item: LeaderBoardItem) => {
+    setSelectedStock(item);
+  };
+
+  const handleAddCompare = (item: LeaderBoardItem) => {
+    addComparedStock(item);
+  };
+
   return (
     <div style={{ padding: '24px' }}>
-      <h1>龙头高度</h1>
-      <p>功能开发中...</p>
+      <Title level={2} style={{ marginBottom: 24 }}>
+        龙头高度
+      </Title>
+
+      <Row gutter={16}>
+        <Col span={14}>
+          <FilterBar />
+          <LeaderBoard
+            onStockSelect={handleStockSelect}
+            onAddCompare={handleAddCompare}
+          />
+        </Col>
+
+        <Col span={10}>
+          <LeaderDetail stock={selectedStock} />
+        </Col>
+      </Row>
     </div>
   );
 }
+
+export default LeaderPage;
